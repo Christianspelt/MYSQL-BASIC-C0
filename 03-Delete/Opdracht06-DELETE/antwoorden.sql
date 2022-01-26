c20ea38453ac41420c994ad3f331c48651844c5f
Christian <Christian.Spelt@student.rocva.nl>
2022-01-26 09:59:51 +0100

    master

--- 03-Delete/Opdracht06-DELETE/antwoorden.sql
+++ 03-Delete/Opdracht06-DELETE/antwoorden.sql
@@ -1,9 +1,10 @@
 -- Opdracht 1 
-
+DELETE FROM player WHERE name = "David Silva";
 -- Opdracht 2 
-
+DELETE FROM player WHERE club = "Willem II";
 -- Opdracht 3
-
+DELETE FROM player WHERE nationality = "belgium" AND club ="FC Barcelona";
 -- Opdracht 4
-
+DELETE FROM player WHERE age >= 38;
 -- Opdracht 5
+DELETE FROM player WHERE id = 167905 or id = 169595 ;
